a88ea7b68d5b3924ece438a0282b9ddd5c056066
jon-perez18 <jonperez102@gmail.com>
2021-03-06 05:56:39 +0000

    Getting ready to use database

--- app.py
+++ app.py
@@ -2,8 +2,23 @@ import os
 from flask import Flask, send_from_directory, json, session
 from flask_socketio import SocketIO
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv()) # This is to load your env variables from .env
 
 app = Flask(__name__, static_folder='./build/static')
+
+# Point SQLAlchemy to your Heroku database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+# IMPORTANT: This must be AFTER creating db variable to prevent
+# circular import issues
+import models
+db.create_all()
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -14,10 +29,10 @@ socketio = SocketIO(
     manage_session=False
 )
 
-players = []
-spectators = []
-PlayerX = ''
-PlayerY = ''
+PLAYERS = []
+SPECTATORS = []
+PLAYERX = ''
+PLAYERY = ''
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
@@ -40,38 +55,36 @@ def on_play(data): # data is whatever arg you pass in your emit call on client
     print(str(data))
     # This emits the 'board' event from the server to all clients except for
     # the client that emmitted the event that triggered this function
-    socketio.emit('board',  data, broadcast=True, include_self=False)
+    socketio.emit('board', data, broadcast=True, include_self=False)
 
 
 @socketio.on('login')
 def on_login(player):
-    print(str(player))
-    global PlayerX
-    global PlayerY
-    if(len(players) < 2):
-        players.append(player['player'])
-        if(len(players) == 1):
-            PlayerX = players[0];
+    global PLAYERX
+    global PLAYERY
+    if len(PLAYERS) < 2:
+        PLAYERS.append(player['player'])
+        if len(PLAYERS) == 1:
+            PLAYERX = PLAYERS[0]
         else:
-            PlayerY = players[1]
+            PLAYERY = PLAYERS[1]
     else:
-        spectators.append(player['player'])
-    print(len(players), players, spectators)
-    
-    data = {"players": players, "spectators": spectators}
-    if(PlayerX != ''):
-        data.update({"PlayerX": PlayerX})
-    if(PlayerY != ''):
-        data.update({"PlayerY": PlayerY})
+        SPECTATORS.append(player['player'])
+    data = {"players": PLAYERS, "spectators": SPECTATORS}
+    if PLAYERX != '':
+        data.update({"PlayerX": PLAYERX})
+    if PLAYERY != '':
+        data.update({"PlayerY": PLAYERY})
     print(str(data))
     socketio.emit('login', data, broadcast=True, include_self=True)
-    # socketio.emit('update', data, broadcast=True, include_self=False)
 
 
+# Note we need to add this line so we can import app in the python shell
+if __name__ == "__main__":
 # Note that we don't call app.run anymore. We call socketio.run with app arg
-socketio.run(
-    app,
-    host=os.getenv('IP', '0.0.0.0'),
-    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
-    debug=True
-)
+    socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
+        debug=True
+    )
